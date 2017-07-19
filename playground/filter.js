1a30a65943a72b58bef7c5cdddae4858473e599d
nebby25 <nebby25@hotmail.com>
2017-07-19 10:39:01 -0600

    Add Users class and updateUserList event

---
+++ playground/filter.js
@@ -0,0 +1,12 @@
+var arr = ['Ben', 'Taylor', 'Jerry', 'Tomlin', 'Savage'];
+
+var user = arr.filter((name)=> na)
+
+
+//        var user = this.getUser(id);
+//        if(user) {
+//           this.users = this.users.filter((user)=> user.id !== id); 
+//        }
+//        return user;
+
+//return this.users.filter((user)=> user.id === id)[0];
